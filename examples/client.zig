763c745874ed1cb973e943482aaadccbe4561428
Matt Hosack <hosack.matt@gmail.com>
2025-03-20 15:56:56 -0700

    Split up client example code to be more readable

--- examples/client.zig
+++ examples/client.zig
@@ -1,18 +1,26 @@
 const std = @import("std");
 const zp = @import("zpotify");
 
+// Build client using environment variables and token file contents
 pub fn buildClient(alloc: std.mem.Allocator) !zp.Client {
     var env_map = try std.process.getEnvMap(alloc);
     defer env_map.deinit();
 
-    const id = env_map.get("ZPOTIFY_ID").?;
-    const secret = env_map.get("ZPOTIFY_SECRET").?;
-    const redirect = env_map.get("ZPOTIFY_REDIRECT").?;
-    const token_file = env_map.get("ZPOTIFY_TOKEN_FILE").?;
+    const token = try buildToken(alloc, env_map.get("ZPOTIFY_TOKEN_FILE").?);
+    defer token.deinit();
 
-    // read Token from TOKEN_FILE
+    return zp.Client.init(alloc, .{
+        .client = env_map.get("ZPOTIFY_ID").?,
+        .secret = env_map.get("ZPOTIFY_SECRET").?,
+        .redirect = env_map.get("ZPOTIFY_REDIRECT").?,
+        .token = token.value,
+    });
+}
+
+// Read contents from token file and return parsed Token
+fn buildToken(alloc: std.mem.Allocator, filename: []const u8) !std.json.Parsed(zp.Token) {
     const f = try std.fs.cwd().openFile(
-        token_file,
+        filename,
         .{ .mode = .read_only },
     );
     defer f.close();
@@ -20,13 +28,5 @@ pub fn buildClient(alloc: std.mem.Allocator) !zp.Client {
     const contents = try f.reader().readAllAlloc(alloc, 2048);
     defer alloc.free(contents);
 
-    var token = try zp.Token.parse(alloc, contents);
-    defer token.deinit();
-
-    return zp.Client.init(alloc, .{
-        .client = id,
-        .secret = secret,
-        .redirect = redirect,
-        .token = token.value,
-    });
+    return try zp.Token.parse(alloc, contents);
 }
